3d0e26d1ca4161bbd5cf2e093a56ce9c3858697c
Jake Bown <jakebown@gmail.com>
2022-09-29 12:15:27 +0100

    Added custom plugin for context.
    Added a new custom plugin which is common during development of a WP site. This plugin can contain custom functionality, credentials and libraries.

---
+++ wp-content/plugins/jakes-custom-plugin/plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Custom Plugin
+ *
+ * @package     Custom Plugin
+ * @author      Custom Plugin
+ * @copyright   2022 Custom Plugin
+ * @license
+ *
+ * @wordpress-plugin
+ * Plugin Name: Custom Plugin
+ * Plugin URI:  https://jakebown.com/plugins/demo-custom
+ * Description: A demo custom plugin
+ * Version:     1.0.0
+ * Author:      Jake Bown
+ * Author URI:  https://jakebown.com
+ * Text Domain: custom-plugin
+ * License:
+ * License URI:
+ */
+
+ define('PLUGIN_NAME', 'Jake\'s Custom Plugin');
+ define('PLUGIN_HOME', __DIR__);
